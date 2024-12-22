cfa3d45cb08c708b1cad423d4144d8a1f37b1696
dashaun <dashaun@dashaun.com>
2024-12-21 23:08:19 -0600

    upgrade to 3.4.1, add spring boot admin client

--- src/main/java/com/javagrunt/service/hello/HelloApplication.java
+++ src/main/java/com/javagrunt/service/hello/HelloApplication.java
@@ -2,6 +2,9 @@ package com.javagrunt.service.hello;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
 public class HelloApplication {
@@ -10,4 +13,17 @@ public class HelloApplication {
 		SpringApplication.run(HelloApplication.class, args);
 	}
 
+}
+
+@Configuration
+class CorsConfig implements WebMvcConfigurer {
+	@Override
+	public void addCorsMappings(CorsRegistry registry) {
+		registry.addMapping("/**")
+				.allowedOrigins("*")
+				.allowedMethods("*")
+				.allowedHeaders("*")
+				.allowCredentials(false)
+				.maxAge(3600);
+	}
 }
